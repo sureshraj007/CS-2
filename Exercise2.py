16ec6c406d433f12f786a7020c974d79de2c6320
SURESH RAJ PERIANNAN <sureshraj@live.com.au>
2017-11-05 19:29:41 +1100

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,13 @@
+# Write a program that uses input to prompt a user for their name and then welcomes them.
+
+# Enter your name: Chuck
+# Hello Chuck
+
+# Program:
+x = input('Enter name: ')
+print('Hello ', x)
+
+# Output: 
+# PS C:\> python.exe '.\Exercise2.py'
+# Enter name: Chuck
+# Hello  Chuck
