c062f32468ad5ab0ba3b2337da0e04e51ef544a3
SURESH RAJ PERIANNAN <sureshraj@live.com.au>
2017-11-05 18:38:06 +1100

    Update add2no.py

--- add2no.py
+++ add2no.py
@@ -1,4 +1,4 @@
-x = input('Enter a number: ')
-y = input('Enter a number: ')
+x = input('Enter the first number: ')
+y = input('Enter the second number: ')
 z= int(x) +  int(y)
-print(z)
+print('The Total number ', z)
