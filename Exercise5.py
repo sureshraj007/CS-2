8d453c85bc7ad6956a271fa3fa706576a9a309d4
SURESH RAJ PERIANNAN <sureshraj@live.com.au>
2017-11-05 19:25:40 +1100

    Create Exercise5.py

---
+++ Exercise5.py
@@ -0,0 +1,6 @@
+# Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit,
+# and print out the converted temperature.
+
+Celsius = input('Enter Celsius: ')
+Fahrenheit = 9.0/5.0 * int(Celsius) + 32
+print('Fahrenheit: ', Fahrenheit)
